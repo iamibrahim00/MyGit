d879401b506560246256ecdac3cd29e1c80b0f90
iamibrahim00 <iyamibrahimshaikh@gmail.com>
2023-01-03 13:55:22 +0530

    dom.js

---
+++ dom.js
@@ -0,0 +1,16 @@
+console.log(document.URL);
+console.log(document);
+
+
+
+//var header = document.getElementById("main-header")
+
+//header.style.borderBottom = "solid 3px black";
+
+
+var cars = document.getElementsByClassName("title3");
+cars[0].style.fontWeight="bold"
+cars[0].style.color="green"
+//for (i=0;i<cars.length;i++){
+//cars.style.backgroundColor="yellow";
+//}
