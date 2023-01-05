85604f60357c6b53ea90bb75a87cf5fe934fdcba
iamibrahim00 <iyamibrahimshaikh@gmail.com>
2023-01-05 12:13:09 +0530

    Delete data from Local Storage

--- BookingApp.js
+++ BookingApp.js
@@ -20,13 +20,22 @@ localStorage.setItem(obj.email, JSON.stringify(obj))
 showUserOnScreen(obj)
 
 function showUserOnScreen(obj){
-  const parentElem = document.getElementById('listOfitems')
+  const parentElement = document.getElementById('listOfitems')
   
-  parentElem.innerHTML = parentElem.innerHTML +`<li> ${obj.name} - ${obj.email} - ${obj.phonenumber}</li>`
-}
+  const childElement = document.createElement('li')
+
+  childElement.textContent = obj.name + ' - ' + obj.email + ' - ' + obj.phonenumber
 
 
 
+  const deletebtn = document.createElement("input")
+  deletebtn.type = "button"
+  deletebtn.value = "Delete"
 
-
-}
+  deletebtn.onclick = () =>{
+    localStorage.removeItem(obj.email)
+    parentElement.removeChild(childElement)
+  }
+  childElement.appendChild(deletebtn)
+  parentElement.appendChild(childElement)
+}}
