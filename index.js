d6f0b330c3d54cca4fd601f16e16a3c408332fd6
iamibrahim00 <iyamibrahimshaikh@gmail.com>
2023-01-15 21:28:48 +0530

    Call, Apply and Bind(by not passing arguments)

--- index.js
+++ index.js
@@ -19,13 +19,24 @@
 //var increaseCount = [10,20]
 //console.log(numOfStudents.apply(Student,increaseCount))
 
-// by using bind
-var Student= {count : 30}
+// by using bind 
+//var Student= {count : 30}
 
-var numOfStudents = function(a,b){
-    return this.count + a + b
-}
+//var numOfStudents = function(a,b){
+    //return this.count + a + b
+//}
 
 //var increaseCount = [10,20]
+//var bound = numOfStudents.bind(Student)
+//console.log(bound(10,20))
+
+//by not passing the argument and calling by this
+var Student= {age: 30}
+
+var numOfStudents = function(){
+    return this.age
+}
+
+
 var bound = numOfStudents.bind(Student)
-console.log(bound(10,20))
+console.log(bound())
