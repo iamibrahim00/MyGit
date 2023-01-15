f22b7c996733b30ffef41514eb1ce7f1b8a9ca77
iamibrahim00 <iyamibrahimshaikh@gmail.com>
2023-01-15 21:26:05 +0530

    Call ,Apply and Bind(Bind)

--- index.js
+++ index.js
@@ -10,11 +10,22 @@
 
 // by using apply
 
+//var Student= {count : 30}
+
+//var numOfStudents = function(a,b){
+ //   return this.count + a + b
+//}
+
+//var increaseCount = [10,20]
+//console.log(numOfStudents.apply(Student,increaseCount))
+
+// by using bind
 var Student= {count : 30}
 
 var numOfStudents = function(a,b){
     return this.count + a + b
 }
 
-var increaseCount = [10,20]
-console.log(numOfStudents.apply(Student,increaseCount))
+//var increaseCount = [10,20]
+var bound = numOfStudents.bind(Student)
+console.log(bound(10,20))
