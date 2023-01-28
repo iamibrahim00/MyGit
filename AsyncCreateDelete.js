8015c2f136863c8d249a52c7a7c52a4a1419cbcb
iamibrahim00 <iyamibrahimshaikh@gmail.com>
2023-01-28 17:58:50 +0530

    AsyncCreateDelete

---
+++ AsyncCreateDelete.js
@@ -0,0 +1,115 @@
+const posts = [
+
+    {title: 'Post One', body:'this post one',createdAt:new Date().getTime()},
+    {title: 'Post Two', body:'this post two',createdAt:new Date().getTime()}
+]
+
+let intervalId = 0
+function getpost(){
+    return new Promise((resolve,reject) =>{
+        clearInterval(intervalId)
+        intervalId =setInterval(() =>{
+         let output =''
+         posts.forEach((post, index) =>{
+            resolve( output += `<li>${post.title} last updated ${(new Date().getTime() - post.createdAt)/1000} seconds ago </li> `)
+     
+         })
+         document.body.innerHTML = output
+     },1000)
+     
+
+    })
+
+   }
+       
+
+function createPost(post){
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            posts.push({...post,createdAt:new Date().getTime()})
+            
+            const error = false
+
+            if(!error){
+                resolve({...post,createdAt:new Date().getTime()})
+            }
+        }, 2000);
+
+    })
+  
+}
+
+function createPost4(post){
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            posts.push({...post,createdAt:new Date().getTime()})
+            
+            const error = false
+
+            if(!error){
+                resolve({...post,createdAt:new Date().getTime()})
+            }else{
+                reject('Error : something went wrong')
+            }
+        }, 4000);
+
+    })
+  
+}
+function deletepost(){
+    return new Promise((resolve, reject) =>{
+        setTimeout(() => {
+            if (posts.length > 0){
+                lastelement = posts.pop()
+                resolve(lastelement)
+            }
+            else{
+                reject('Array is now empty')
+            }
+        
+        
+        }, 3000);
+
+    })
+
+}
+
+
+createPost({title:'Post Three', body:'this is post three', createdAt:new Date().getTime()})
+
+//.then(() =>{
+  //  getpost()
+    //deletepost().then((deletedelement) =>{
+      //  console.log('this is deleted',deletedelement)
+        //getpost()
+       // deletepost().then((deletedelement) => {
+         //   console.log('this is deleted',deletedelement)
+           // getpost()
+            //deletepost().then((deletedelement) => {
+              //  console.log('this is deleted',deletedelement)
+               // getpost()
+                //deletepost().then((deletedelement) => {
+                  //  console.log('this is deleted',deletedelement)
+                //deletepost().then(() => { })
+                  //  .catch((err) => {
+                    //        console.log('inside catch block ',err)
+                    //})
+                //})
+           // }).catch((err) => {console.log("success deleted",err)})
+        //}).catch((err) => {console.log("success deleted",err)})
+    //}).catch((err) => {console.log("success deleted",err)})
+//})
+createPost4({title:'Post Four', body:'this is post Four', createdAt:new Date().getTime()})
+
+
+
+async function Func1(){
+    const msg = await getpost()
+    console.log(msg)
+
+    const del = await deletepost()
+    console.log(del)
+ 
+}
+
+Func1()
