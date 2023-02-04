c65eadae45f3e152b465a0b3acc801c953dc0583
iamibrahim00 <iyamibrahimshaikh@gmail.com>
2023-02-04 19:31:17 +0530

    DeleteUser1

---
+++ CrudCrudTask/DeleteUser.js
@@ -0,0 +1,82 @@
+
+
+
+function saveToLocalStorage(event) {
+  
+ 
+    event.preventDefault();
+    const name = event.target.name.value;
+  
+    const email = event.target.email.value;
+    const phonenumber = event.target.phonenumber.value;
+  
+    
+    const obj={
+    name,
+    email,
+    phonenumber
+    }
+  
+    axios.post("https://crudcrud.com/api/930df1ca24b0406eb09a94dca4558b01/appointmentData",obj)
+    .then((response) => {
+        showUserOnScreen(response.data)
+        //console.log(response)
+    }).catch(err => {
+        document.body.innerHTML =document.body.innerHTML + "<h4>Something Went wrong </h4>"
+    })
+
+  
+  //localStorage.setItem(obj.email, JSON.stringify(obj))
+  //showUserOnScreen(obj)
+  }
+  //store obj on localstorage
+
+  // window.addEventListener("DOMContentLoaded", () =>{
+  //   const localStorageObj =localStorage
+  //   const localStoragKeys = Object.keys(localStorageObj)
+
+  //   for(var i=0; i<localStoragKeys.length;i++){
+  //     const key  =localStoragKeys[i]
+  //     const userdetailsString =localStorageObj[key]
+  //     const userdetailsobj = JSON.parse(userdetailsString)
+  //     showUserOnScreen(userdetailsobj)
+  //   }
+  // })
+  window.addEventListener("DOMContentLoaded", ()=>{
+    axios.get("https://crudcrud.com/api/930df1ca24b0406eb09a94dca4558b01/appointmentData")
+    .then((response) =>{
+      console.log(response)
+  
+      for(var i =0; i< response.data.length;i++){
+       showUserOnScreen(response.data[i])
+      }
+    }).catch((err) => console.log(err))
+  
+  })
+
+  function showUserOnScreen(obj){
+    const parentElement = document.getElementById('listOfitems')
+    
+    const childElement = document.createElement('li')
+  
+    childElement.textContent = obj.name + ' - ' + obj.email + ' - ' + obj.phonenumber
+
+  
+    const deletebtn = document.createElement("input")
+    deletebtn.type = "button"
+    deletebtn.value = "Delete"
+    
+    
+    deletebtn.onclick = () =>{
+     axios.delete("https://crudcrud.com/api/930df1ca24b0406eb09a94dca4558b01/appointmentData/63de644081b3ab03e8473d75")
+      parentElement.removeChild(childElement)
+    }  
+  
+    childElement.appendChild(deletebtn)
+    
+    parentElement.appendChild(childElement)
+  
+  }
+  
+  
+  
